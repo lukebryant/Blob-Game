4be11516cb761688f03d02d01dceebb26787282c
mlb <mlb>
2016-06-04 20:17:24 +0100

    Implement glitchy swordfighting and control by id

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	private Vector3 target;
-	public float speed = 1.5f;
+    [SerializeField]
+    float speed = 1.5f;
+    [SerializeField]
+    int id;
+    private bool active;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		target = transform.position;
+        if (id == 0) active = true;
+        else active = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-		
+        if (Input.GetKey("0"))
+        {
+            if (id == 0) active = true;
+            else active = false;
+        }
+        if (Input.GetKey("1"))
+        {
+            if (id == 1) active = true;
+            else active = false;
+        }
+        if (!active) return;
 		var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		var target = new Vector3(mousePos.x,mousePos.y,transform.position.z);
 
@@ -22,8 +37,5 @@ public class PlayerMovement : MonoBehaviour {
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
-
-         
-
 	}
 }
