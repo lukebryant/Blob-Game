60c6772a2ccd1f344e46bc93123dad40008ba663
Borissssss <richy.weight8@gmail.com>
2016-06-04 22:09:02 +0100

    add slimes and random move direction

---
+++ Assets/SlimeMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeMovement : MonoBehaviour
+{
+    private Vector3 direction;
+    public float speed = 1.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        direction = (new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0.0f)).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position += direction * speed * Time.deltaTime;
+
+
+    }
+}
