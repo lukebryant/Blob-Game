a58222d6ed0ebda4b7c5e1d4aebe54f9247fed54
mlb <mlb>
2016-06-04 17:31:48 +0100

    Add background and player that moves towards mouse on click

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private Vector3 target;
+	public float speed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target = new Vector3(target.x,target.y,transform.position.z);
+		}
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}
+}
