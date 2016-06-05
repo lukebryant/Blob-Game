1145ad9265d11af7348c9cc81ed878698ea259da
Borissssss <richy.weight8@gmail.com>
2016-06-05 19:23:28 +0100

    spawn slimes

--- Assets/SlimeSpawn.cs
+++ Assets/SlimeSpawn.cs
@@ -5,6 +5,8 @@ public class SlimeSpawn : MonoBehaviour
 {
     public GameObject[] enemies;
 
+    public GameObject Green_slime;
+
     public int total;
 
     private Vector3 spawnPoint;
@@ -14,6 +16,39 @@ public class SlimeSpawn : MonoBehaviour
         //find the amount of enemies in the scene
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         total = enemies.Length;
-
+        
+        if (total <40)
+        {
+            InvokeRepeating("spawnEnemy", 5, 10f);
+        }
 	}
+    void spawnEnemy ()
+    {
+        float z = Random.Range(0.0f, 4.0f);
+        if(z<=1)
+        {
+            Vector3 pos = new Vector3(Random.Range(0.0f, 1.0f), -0.1f, 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+            Instantiate(Green_slime, pos, Quaternion.identity);
+     
+        }
+        if(z<=2)
+        {
+            Vector3 pos = new Vector3(Random.Range(0.0f, 1.0f), 1.1f, 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+            Instantiate(Green_slime, pos, Quaternion.identity);
+        }
+        if (z <= 3)
+        {
+            Vector3 pos = new Vector3(1.1f, Random.Range(0.0f, 1.0f), 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+            Instantiate(Green_slime, pos, Quaternion.identity);
+        }
+        if(z<= 4)
+        {
+            Vector3 pos = new Vector3(-0.1f, Random.Range(0.0f, 1.0f), 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+            Instantiate(Green_slime, pos, Quaternion.identity);
+        }
+    }
 }
