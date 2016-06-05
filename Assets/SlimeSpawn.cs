aed1a967e92c2a0209c812cffd8ddeaa17c14524
Borissssss <richy.weight8@gmail.com>
2016-06-05 16:04:37 +0100

    add slime spawn manager

---
+++ Assets/SlimeSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeSpawn : MonoBehaviour
+{
+    public GameObject[] enemies;
+
+    public int total;
+
+    private Vector3 spawnPoint;
+
+	void Update ()
+    {
+        //find the amount of enemies in the scene
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        total = enemies.Length;
+
+	}
+}
