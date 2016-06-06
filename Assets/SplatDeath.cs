de020ec8b9568ce5fbd3a98f2a1525231982e80d
Borissssss <richy.weight8@gmail.com>
2016-06-06 18:53:26 +0100

    add splat death

---
+++ Assets/SplatDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplatDeath : MonoBehaviour {
+    public int destroyTime;
+	// Use this for initialization
+	void Start () {
+
+		Destroy (this.gameObject, destroyTime);
+    }
+	
+}
