e05599973a5b18d3cee07f63f65a0b616d09ae07
Borissssss <richy.weight8@gmail.com>
2016-06-06 20:23:09 +0100

    add score counter

---
+++ Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public int score;
+	public GUIText scoreText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddScore (int newScore)
+	{
+		score += newScore;
+		UpdateScore();
+	}	
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
--- Assets/SlimeMovement.cs
+++ Assets/SlimeMovement.cs
@@ -7,11 +7,17 @@ public class SlimeMovement : MonoBehaviour
     private GameObject splat;
     public Vector2 startingDirection;
     public float speed = 1.5f;
+	public int scoreValue;
+	public GameController gameController;
 
     // Use this for initialization
     void Start()
     {
-        GetComponent<Rigidbody2D>().AddForce(startingDirection);
+		//Each instance gets a reference to the game controller
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("Manager");
+		gameController = gameControllerObject.GetComponent<GameController> ();
+
+		GetComponent<Rigidbody2D>().AddForce(startingDirection);
     }
 
     // Update is called once per frame
@@ -24,5 +30,6 @@ public class SlimeMovement : MonoBehaviour
         GameObject newSplat = (GameObject)Instantiate(splat);
         newSplat.transform.position = this.transform.position;
 		Destroy (gameObject);
+		gameController.AddScore (scoreValue);
     }
 }
